c2a479c63b145fb23476b69862d9fa26e61500f3
dzsofi20 <44711746+dzsofi20@users.noreply.github.com>
2023-06-06 19:26:16 +0200

    Update app_.py

--- app_.py
+++ app_.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from predict_page import show_predict_page
-from explore_page import show_explore_page
+from predict_page_ import show_predict_page
+from explore_page_ import show_explore_page
 
 page = st.sidebar.selectbox("Explore or Predict from different input data", ("Survival prediction", "Explore dataset"))
 
